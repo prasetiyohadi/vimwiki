0c7a3f62506717a76f66c350ace93ee5a158bb50
Prasetiyo Hadi Purwoko <pras@deuterion.net>
2021-06-06 09:15:01 +0700

    Update generate-html.sh script and Dockerfile to support mkdocs

--- generate-html.sh
+++ generate-html.sh
@@ -1,13 +1,31 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-main() {
-    mkdir -p public
-    cp style.css public
-    cp -r images public
-    find . -regex './.*.md' -exec sh \
-        -c 'pandoc -f markdown -t html -o public/${0%.md}.html -s --css style.css --lua-filter=links-to-html.lua ${0}' \
+BUILDER=${BUILDER:-use_mkdocs}
+
+clean() {
+    rm -fr site
+}
+
+use_mkdocs() {
+    mkdir -p docs
+    cp -r images docs
+    find . -maxdepth 1 -type f \( -iname "*.md" ! -iname "README.md" \) -exec cp {} docs \;
+    mkdocs build
+}
+
+use_pandoc() {
+    mkdir -p site
+    cp style.css site
+    cp -r images site
+    find . -maxdepth 1 -type f \( -iname "*.md" ! -iname "README.md" \) -exec sh \
+        -c 'pandoc -f markdown -t html -o site/${0%.md}.html -s --css style.css --lua-filter=links-to-html.lua ${0}' \
         {} \;
+}
+
+main() {
+    clean
+    eval "$BUILDER"
 }
 
 main
